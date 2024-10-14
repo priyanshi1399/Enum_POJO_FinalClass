453683ccb50e6ab9770dc623bd70c47122e8f01a
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-14 14:06:41 +0530

    SamplePOJO

---
+++ src/StudentPOJO.java
@@ -0,0 +1,34 @@
+public class StudentPOJO {
+
+    int name;
+    private int rollNumber;
+    protected String address;
+
+    public int getname(){
+        return name;
+    }
+
+    public void setname(int name){
+        this.name=name;
+    }
+
+    public int getRollNumber(){
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber){
+        this.rollNumber=rollNumber;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address=address;
+    }
+
+}
+
+//where it is used--->day to day where
+//when request comes to our syste\
