e57580b93d241b089923c3bcb31e3a056c641a55
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-17 13:53:43 +0530

    enum_Singleton

---
+++ src/EDBConnection.java
@@ -0,0 +1,7 @@
+public enum EDBConnection {
+
+    INSTANCE;
+}
+//jvm takes care one object per JVM.
+//enum takes care only one instance per JVM.
+//it is private constructor also.[-[-------------------------------
